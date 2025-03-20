0ef726e38e79ad0bfdc89307a5c82a6638cd37de
Vlad Zagvozdkin <v.zagvozdkin@famedly.com>
2025-03-21 01:18:13 +0500

    feat!: Set OTEL resource to proper package and version

---
+++ src/gen_config_schema.rs
@@ -0,0 +1,10 @@
+//! Generates config schema for this crate:
+//! ```sh
+//! cargo run --features serde_yaml,schemars --bin gen-config-schema > config-schema.yaml
+//! ```
+#![allow(clippy::unwrap_used, clippy::print_stdout)]
+
+fn main() {
+	let schema = schemars::schema_for!(rust_telemetry::config::OtelConfig);
+	print!("{}", serde_yaml::to_string(&schema).unwrap());
+}
