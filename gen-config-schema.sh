163e89324bd9843abf7c1c05c922860f7876c329
Emelie Graven <e.graven@famedly.com>
2025-04-28 15:04:23 +0200

    ci: Add reuse pre-commit hook and CI job

--- gen-config-schema.sh
+++ gen-config-schema.sh
@@ -3,8 +3,13 @@
 # SPDX-FileCopyrightText: 2025 Famedly GmbH (info@famedly.com)
 #
 # SPDX-License-Identifier: Apache-2.0
+cat <<EOF > config-schema.yaml
+# SPDX-FileCopyrightText: 2025 Famedly GmbH (info@famedly.com)
+#
+# SPDX-License-Identifier: Apache-2.0
+EOF
 
-cargo run -q --features serde_yaml,schemars --bin gen-config-schema > config-schema.yaml
+cargo run -q --features serde_yaml,schemars --bin gen-config-schema >> config-schema.yaml
 
 if [ ! -z "$(git diff --name-only 'config-schema.yaml' 2>&1)" ]; then
 	echo "config-schema.yaml has changed, try adding and committing it again"
